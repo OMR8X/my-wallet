32e022e0c4f8065a0ceaa0d9b27e2ccaf352dd06
عمر <omar@alMacBook-Pro-alkhas-bmr.local>
2023-09-18 18:14:19 +0300

    finish data 1

--- lib/features/transactions/data/data_sources/transactions_ds.dart
+++ lib/features/transactions/data/data_sources/transactions_ds.dart
@@ -1,15 +1,69 @@
-import 'package:sqflite/sqflite.dart';
+import 'package:my_wallet/features/transactions/domain/entities/expense.dart';
+import 'package:my_wallet/features/transactions/domain/entities/income.dart';
+import 'package:my_wallet/features/transactions/domain/entities/transaction.dart';
+import 'package:sqflite/sqflite.dart' as sqflite;
 
 abstract class TransactionDataSource {
-  getLatestTransactions();
+  // Getters
+  Future<List<Transaction>> getRecentTransactions();
+  Future<List<Income>> getAllIncomes();
+  Future<List<Expense>> getAllExpenses();
+  Future<(double, double)> getExpensesAndIncomeAmount();
+  // Adders
+  Future<void> addIncome(Income i);
+  Future<void> addExpence(Expense e);
+  // updaters
+  Future<void> updateTransaction(Transaction t);
+  //Deleters
+  Future<void> deleteTransaction(Transaction t);
 }
 
 class TransactionDataSourceImpl implements TransactionDataSource {
-  final Database database;
+  final sqflite.Database database;
   TransactionDataSourceImpl({required this.database});
+  
   @override
-  Future<List<Transaction>> getLatestTransactions() async {
-    await database.rawQuery("SELECT * FROM dataBaseName");
-    return [];
+  Future<void> addExpence(e) {
+    throw UnimplementedError();
   }
+  
+  @override
+  Future<void> addIncome(i) {
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<void> deleteTransaction(Transaction t) {
+    throw UnimplementedError();
+  }
+  
+
+  
+  @override
+  Future<(double, double)> getExpensesAndIncomeAmount() {
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<List<Transaction>> getRecentTransactions() {
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<void> updateTransaction(Transaction t) {
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<List<Expense>> getAllExpenses() {
+    // TODO: implement getAllExpenses
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<List<Income>> getAllIncomes() {
+    // TODO: implement getAllIncomes
+    throw UnimplementedError();
+  }
+
 }
--- lib/features/transactions/domain/usecases/get_recent_transactions.dart
+++ lib/features/transactions/domain/usecases/get_recent_transactions.dart
@@ -1,9 +1,9 @@
-import 'package:my_wallet/features/transactions/domain/repositories/trans_repository.dart';
-
+import '../repositories/trans_repository.dart';
 import '../entities/transaction.dart';
 
 class GetRecentTranactionsUS {
   final TransactionsRepository repository;
+
   GetRecentTranactionsUS(this.repository);
   Future<List<Transaction>> call() async {
     return await repository.getRecentTransactions();
