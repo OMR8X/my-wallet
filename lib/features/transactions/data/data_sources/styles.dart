0910d919c516963f9882427c7327db5531a33df6
Aboud-Khalaf <aboudkalaf10@gmail.com>
2023-09-18 16:34:09 +0300

    convert if_else to switch in get Style by category

--- lib/features/transactions/data/data_sources/styles.dart
+++ lib/features/transactions/data/data_sources/styles.dart
@@ -1,76 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:my_wallet/features/transactions/domain/entities/trans_style.dart';
 
-getStyleByCategory(String category) {
-  if (category.contains("bills")) {
-    return TransStyle(icon: "icon", color: const Color(0xff0073E6));
+TransStyle getStyleByCategory(String category) {
+  switch (category) {
+    case "bills":
+      return TransStyle(icon: "icon", color: const Color(0xff0073E6));
+    case "cars":
+      return TransStyle(icon: "icon", color: const Color(0xffFF6600));
+    case "clothes":
+      return TransStyle(icon: "icon", color: const Color(0xffFF3399));
+    case "delivery":
+      return TransStyle(icon: "icon", color: const Color(0xff33CC33));
+    case "food_and_drinks":
+      return TransStyle(icon: "icon", color: const Color(0xffFFCC00));
+    case "entertainment":
+      return TransStyle(icon: "icon", color: const Color(0xff9900CC));
+    case "gifts":
+      return TransStyle(icon: "icon", color: const Color(0xffFF99CC));
+    case "health":
+      return TransStyle(icon: "icon", color: const Color(0xff0099CC));
+    case "house":
+      return TransStyle(icon: "icon", color: const Color(0xffFF9933));
+    case "animals":
+      return TransStyle(icon: "icon", color: const Color(0xff66CC99));
+    case "sports":
+      return TransStyle(icon: "icon", color: const Color(0xffFF3333));
+    case "transport":
+      return TransStyle(icon: "icon", color: const Color(0xffDFBC40));
+    case "care":
+      return TransStyle(icon: "icon", color: const Color(0xffFF66E6));
+    case "shopping":
+      return TransStyle(icon: "icon", color: const Color(0xffFF0066));
+    case "housing":
+      return TransStyle(icon: "icon", color: const Color(0xff3366FF));
+    case "private_job":
+      return TransStyle(icon: "icon", color: const Color(0xffFF6600));
+    case "salary":
+      return TransStyle(icon: "icon", color: const Color(0xff0073E6));
+    default:
+       return TransStyle(icon: "icon", color: Colors.grey);
   }
-  if (category.contains("cars")) {
-    return TransStyle(icon: "icon", color: const Color(0xffFF6600));
-  }
-  if (category.contains("clothes")) {
-    return TransStyle(icon: "icon", color: const Color(0xffFF3399));
-  }
-  if (category.contains("delivery")) {
-    return TransStyle(icon: "icon", color: const Color(0xff33CC33));
-  }
-  if (category.contains("food_and_drinks")) {
-    return TransStyle(icon: "icon", color: const Color(0xffFFCC00));
-  }
-  if (category.contains("entertainment")) {
-    return TransStyle(icon: "icon", color: const Color(0xff9900CC));
-  }
-  if (category.contains("gifts")) {
-    return TransStyle(icon: "icon", color: const Color(0xffFF99CC));
-  }
-  if (category.contains("health")) {
-    return TransStyle(icon: "icon", color: const Color(0xff0099CC));
-  }
-  if (category.contains("house")) {
-    return TransStyle(icon: "icon", color: const Color(0xffFF9933));
-  }
-  if (category.contains("animals")) {
-    return TransStyle(icon: "icon", color: const Color(0xff66CC99));
-  }
-  if (category.contains("sports")) {
-    return TransStyle(icon: "icon", color: const Color(0xffFF3333));
-  }
-  if (category.contains("transport")) {
-    return TransStyle(icon: "icon", color: const Color(0xffDFBC40));
-  }
-  if (category.contains("care")) {
-    return TransStyle(icon: "icon", color: const Color(0xffFF66E6));
-  }
-  if (category.contains("shopping")) {
-    return TransStyle(icon: "icon", color: const Color(0xffFF0066));
-  }
-  if (category.contains("housing")) {
-    return TransStyle(icon: "icon", color: const Color(0xff3366FF));
-  }
-  //
-  if (category.contains("private_job")) {
-    return TransStyle(icon: "icon", color: const Color(0xffFF6600));
-  }
-  if (category.contains("salary")) {
-    return TransStyle(icon: "icon", color: const Color(0xff0073E6));
-  }
-  if (category.contains("gifts")) {
-    return TransStyle(icon: "icon", color: const Color(0xffFF99CC));
-  }
-  if (category.contains("sale")) {
-    return TransStyle(icon: "icon", color: const Color(0xffFFCC00));
-  }
-  if (category.contains("expenses")) {
-    return TransStyle(icon: "icon", color: const Color(0xffFF3399));
-  }
-  if (category.contains("reward")) {
-    return TransStyle(icon: "icon", color: const Color(0xff33CC33));
-  }
-//
-  return TransStyle(icon: "icon", color: Colors.grey);
 }
-// type color
 
+// type color
 // bills   0073E6
 // Cars    FF6600
 // clothes  FF3399
@@ -86,8 +58,6 @@ getStyleByCategory(String category) {
 // care  FF66E6
 // shopping  FF0066
 // housing  3366FF
-
-
 // private_job  FF6600
 // salary     0073E6
 // gifts   FF99CC
