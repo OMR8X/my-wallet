f677cdc2ca859cfa53921f4b5c8dcea436e0a257
Aboud-Khalaf <aboudkalaf10@gmail.com>
2023-09-26 15:16:21 +0300

    Create images healper

--- lib/core/helpers/assets/images_h.dart
+++ lib/core/helpers/assets/images_h.dart
@@ -1,11 +1,23 @@
 const String expenseRoot = 'assets/pictures/icons/expense';
-const String incomesRoot = 'assets/pictures/icons/expense';
+const String incomesRoot = 'assets/pictures/icons/income';
 
 class ExpensesIcons {
-  static const String startHere = "$expenseRoot/";
+  static const bills = "$expenseRoot/bills.svg";
+  static const cars = "$expenseRoot/cars.svg";
+  static const clothes = "$expenseRoot/clothes.svg";
+  static const foodAndDrink = "$expenseRoot/food&drinks.svg";
+  static const entertainment = "$expenseRoot/entertainment.svg";
+  static const gifts = "$expenseRoot/gifts.svg";
+  static const health = "$expenseRoot/health.svg";
+  static const house = "$expenseRoot/house.svg";
+  static const animals = "$expenseRoot/animals.svg";
+  static const sport = "$expenseRoot/sport.svg";
+  static const transport = "$expenseRoot/transport.svg";
+  static const care = "$expenseRoot/care.svg";
+  static const shopping = "$expenseRoot/shopping.svg";
 }
 
 class IncomeIcons {
-  String root = 'assets/pictures/icons/income/';
-  static const String startHere = "$incomesRoot/";
+  static const privateJob = "$incomesRoot/private_job.svg";
+  static const salary = "$incomesRoot/salary.svg";
 }
